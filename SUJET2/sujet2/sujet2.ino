614def79d9d3266e184db53ab4a5abd88260c1ae
slashformotion <slashformotion@protonmail.com>
2020-09-19 10:33:50 +0200

    format

--- SUJET2/sujet2/sujet2.ino
+++ SUJET2/sujet2/sujet2.ino
@@ -3,28 +3,30 @@ const int I1 = 2, I2 = 3, L = 10, S = 11; // interrupteurs 1 et 22, led, sortie
 bool v1, v2;
 int x;
 int periode_ms;
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(I1, INPUT);
   pinMode(I2, INPUT);
   pinMode(L, OUTPUT);
   pinMode(S, OUTPUT);
-
-
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 
   // acquisition des données
   v1 = bool(digitalRead(I1));
   v2 = bool(digitalRead(I2));
-  Serial.print(v1); Serial.print("   "); Serial.println(v2);
-
+  Serial.print(v1);
+  Serial.print("   ");
+  Serial.println(v2);
 
   // logique
-  if (v1== false) {
+  if (v1 == false)
+  {
     digitalWrite(L, HIGH); // on allume la LED
     digitalWrite(S, HIGH); // on allume la sortie
 
@@ -33,21 +35,24 @@ void loop() {
     digitalWrite(S, LOW); // on éteint la sortie
 
     delayMicroseconds(50 / 2);
-
   }
-  else {
+  else
+  {
     // on s'occupe d'abord de la led
-    if ((millis() % 1000) < 500) {
+    if ((millis() % 1000) < 500)
+    {
       digitalWrite(L, HIGH);
     }
-    else {
+    else
+    {
       digitalWrite(L, LOW);
     }
 
     // acquisition de la valeur variable du potentiomêtre
     x = map(analogRead(A0), 0, 1023, 100, 500);
     periode_ms = (1 / x) * 1000000;
-    if(v2==false){
+    if (v2 == false)
+    {
       digitalWrite(S, HIGH);
 
       delayMicroseconds(periode_ms / 4);
@@ -55,9 +60,9 @@ void loop() {
       digitalWrite(S, LOW);
 
       delayMicroseconds(periode_ms * (3 / 4));
-
     }
-    else {
+    else
+    {
       digitalWrite(S, HIGH);
 
       delayMicroseconds(periode_ms * (3 / 4));
@@ -65,11 +70,6 @@ void loop() {
       digitalWrite(S, LOW);
 
       delayMicroseconds(periode_ms / 4);
-
     }
-
   }
-
-
-
 }
