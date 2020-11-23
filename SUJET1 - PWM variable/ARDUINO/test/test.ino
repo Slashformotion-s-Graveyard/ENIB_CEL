8d5303a89b3a08ae87fa15bf64d558bc17993fd7
slashformotion <slashformotion@protonmail.com>
2020-11-23 15:36:21 +0100

    upd: for debug

--- SUJET1 - PWM variable/ARDUINO/test/test.ino
+++ SUJET1 - PWM variable/ARDUINO/test/test.ino
@@ -19,6 +19,7 @@ void setup()
     pinModeFast(L, OUTPUT);
     pinModeFast(I1, INPUT);
     pinModeFast(I2, INPUT);
+    Serial.begin();
 }
 
 void loop()
@@ -26,11 +27,13 @@ void loop()
     lastTime = curTime;
     curTime = micros();
     v1 = digitalReadFast(I1);
+    Serial.print("Etat de I1 (sur la broche 2) = "); Serial.println(v1)
     last_s = s;
     last_led = led;
 
     if (!v1)
-    {
+    {   
+        
         front_montant_v2 = true;
         if (front_montant_v1)
         {
@@ -51,6 +54,8 @@ void loop()
     {
         front_montant_v1 = true;
         v2 = digitalReadFast(I2);
+        Serial.print("Etat de 1 (sur la broche 3) = "); 
+        Serial.print("Etat des timers: led_timer="); Serial.print(led_timer); Serial.print(" s_00_01_timer = "); Serial.print(s_00_01_timer); ; Serial.print(" s_10_timer = "); Serial.print(s_10_timer);  ; Serial.print(" s_11_timer = "); Serial.println(s_11_timer); 
         period = int((analogRead(PIN_A_POTAR) / 1023) * 8000 + 2000);
 
         if (front_montant_v2)
@@ -95,6 +100,7 @@ void loop()
     // SORTIE: action
     if (s != last_s)
     {
+        Serial.print("En passant ici on switch la Sortie vers S = "); Serial.println(S)
         if (s)
         {
             digitalWriteFast(S, HIGH);
@@ -108,6 +114,7 @@ void loop()
     // LED: action
     if (led != last_led)
     {
+        Serial.print("En passant ici on switch la led vers led = "); Serial.println(led)
         if (led)
         {
             digitalWriteFast(L, HIGH);
