955b007252e56b1931377ca5f97be832bafb993b
slashformotion <slashformotion@protonmail.com>
2020-11-23 15:44:06 +0100

    debug ended

--- SUJET1 - PWM variable/ARDUINO/test/test.ino
+++ SUJET1 - PWM variable/ARDUINO/test/test.ino
@@ -27,13 +27,23 @@ void loop()
     lastTime = curTime;
     curTime = micros();
     v1 = digitalReadFast(I1);
-    Serial.print("Etat de I1 (sur la broche 2) = "); Serial.println(v1)
+    Serial.print("Etat de I1 (sur la broche 2) = ");
+    Serial.println(v1);
+    Serial.print("Etat des timers: led_timer=");
+    Serial.print(led_timer);
+    Serial.print(" s_00_01_timer = ");
+    Serial.print(s_00_01_timer);
+    Serial.print(" s_10_timer = ");
+    Serial.print(s_10_timer);
+    ;
+    Serial.print(" s_11_timer = ");
+    Serial.println(s_11_timer);
     last_s = s;
     last_led = led;
 
     if (!v1)
-    {   
-        
+    {
+
         front_montant_v2 = true;
         if (front_montant_v1)
         {
@@ -54,8 +64,8 @@ void loop()
     {
         front_montant_v1 = true;
         v2 = digitalReadFast(I2);
-        Serial.print("Etat de 1 (sur la broche 3) = "); 
-        Serial.print("Etat des timers: led_timer="); Serial.print(led_timer); Serial.print(" s_00_01_timer = "); Serial.print(s_00_01_timer); ; Serial.print(" s_10_timer = "); Serial.print(s_10_timer);  ; Serial.print(" s_11_timer = "); Serial.println(s_11_timer); 
+        Serial.print("Etat de I2 (sur la broche 3) = ");
+        (s_11_timer);
         period = int((analogRead(PIN_A_POTAR) / 1023) * 8000 + 2000);
 
         if (front_montant_v2)
@@ -64,10 +74,10 @@ void loop()
             s_11_timer = 0;
             led_timer = 0;
         }
-        
-        if (led_timer >= 500000) {
+
+        if (led_timer >= 500000)
+        {
             led = !led;
-            
         }
 
         if (!v2)
@@ -78,7 +88,7 @@ void loop()
             }
             else if (!s && s_10_timer >= int(period * 0.75))
             {
-                s = true
+                s = true;
             }
             s_10_timer = s_10_timer + curTime - lastTime;
         }
@@ -90,7 +100,7 @@ void loop()
             }
             else if (!s && s_11_timer >= int(period * 0.25))
             {
-                s = true
+                s = true;
             }
             s_11_timer = s_11_timer + curTime - lastTime;
         }
@@ -100,7 +110,8 @@ void loop()
     // SORTIE: action
     if (s != last_s)
     {
-        Serial.print("En passant ici on switch la Sortie vers S = "); Serial.println(S)
+        Serial.print("En passant ici on switch la Sortie vers S = ");
+        Serial.println(s);
         if (s)
         {
             digitalWriteFast(S, HIGH);
@@ -114,7 +125,8 @@ void loop()
     // LED: action
     if (led != last_led)
     {
-        Serial.print("En passant ici on switch la led vers led = "); Serial.println(led)
+        Serial.print("En passant ici on switch la led vers led = ");
+        Serial.println(led);
         if (led)
         {
             digitalWriteFast(L, HIGH);
